93468dceba70bcc56ee16abb07a755d61f0a0ee7
thaiduong4990 <duongthai646@gmail.com>
2025-07-25 20:57:07 +0700

    Add files via upload
    Signed-off-by: thaiduong4990 <duongthai646@gmail.com>

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function foodHack() {
-  const foods = ["Chǎofàn (Cơm chiên)", "Jiǎozi (Sủi cảo)", "Miàntiáo (Mì)", "Bāozi (Bánh bao)", "Làzhī jī (Gà cay Tứ Xuyên)"];
-  const randomFood = foods[Math.floor(Math.random() * foods.length)];
-  document.getElementById("food-result").innerText = `Hệ thống đề xuất món: ${randomFood}! 😎`;
+  const loading = document.getElementById("loading");
+  const result = document.getElementById("food-result");
+  loading.style.display = "block"; // Hiển thị loading
+  result.innerText = ""; // Xóa kết quả cũ
   document.body.style.backgroundColor = "#111";
+
   setTimeout(() => {
+    const foods = ["Chǎofàn (Cơm chiên)", "Jiǎozi (Sủi cảo)", "Miàntiáo (Mì)", "Bāozi (Bánh bao)", "Làzhī jī (Gà cay Tứ Xuyên)"];
+    const randomFood = foods[Math.floor(Math.random() * foods.length)];
+    result.innerText = `Hệ thống đề xuất món: ${randomFood}! 😎`;
+    loading.style.display = "none"; // Ẩn loading
     document.body.style.backgroundColor = "#000";
-  }, 500);
+  }, 2000); // Chờ 30 giây để mô phỏng loading
 }
