4c221fff89733ff585a3ee05daf584d00cb0e6f8
thaiduong4990 <duongthai646@gmail.com>
2025-07-26 20:44:13 +0700

    Add files via upload
    Signed-off-by: thaiduong4990 <duongthai646@gmail.com>

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function hackProfile() {
   }, 500);
 
   // Thêm âm thanh
-  const audio = new Audio("duong.wva"); // Thay bằng tên file âm thanh của cậu
+  const audio = new Audio("duong.wav"); // Thay bằng tên file âm thanh của cậu
   audio.play(); // Phát âm thanh khi bấm nút
 
   loading.style.display = "block";
